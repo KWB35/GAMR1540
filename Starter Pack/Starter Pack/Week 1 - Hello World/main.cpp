4fd7b1a814139bb097dfc255327729e60a8a574b
Kian Button <P2869497@LEC-ADMIN.dmu.ac.uk>
2025-04-01 12:58:07 +0100

    Pushing the C++ fundemental starter pack

---
+++ Starter Pack/Starter Pack/Week 1 - Hello World/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //Using someone elses code via iostream.h
+
+using namespace std;//using std nameplate gives ease of access to things in iostream
+
+int main() //initialises the function main() 
+{
+    cout << "Hello World!\n" << endl; //prints "Hello World!" to the console window and makes it so that anything printed afterwards in on a seperate line
+    
+    system("PAUSE"); //pauses the program
+    return 0; //Tells the OS the program ran succesfully and allows for the program to detect errors as if you return anything other than zero an error has occured
+}
+
