fde61ea33d68a8b22c2a8e0d17f8e1e17e40c1c0
KWB35 <kianbutts@icloud.com>
2025-05-01 12:59:15 +0100

    More work on week 2 parcel task

--- Starter Pack/Starter Pack/Week 2 - Paul's Parcels/main.cpp
+++ Starter Pack/Starter Pack/Week 2 - Paul's Parcels/main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int main()
 {
-    int iParcelWeight = 25; //
-    int iMaxCapacity = 1450;
-    int iHeaviestVan = 0;
-    int iNumVans = 0;
-    int iBeltCapacity = 10;
+    int iParcelWeight = 25; //to be changed to a user input 
+    int iMaxCapacity = 1450; //to be changed to a user input
+    int iHeaviestVan = 0;//to be changed to a user input
+    int iNumVans = 0; //to be changed to a user input
+    int iBeltCapacity = 10; //to be changed to a user input
 
     while (iBeltCapacity > 0) 
     {
-        int iPayload = 0;
+        int iPayload = 0; //to be changed to a user input
         while (iPayload + iParcelWeight <= iMaxCapacity) 
         {
             iPayload = iPayload + iParcelWeight;
