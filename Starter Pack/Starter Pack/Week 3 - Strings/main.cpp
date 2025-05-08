16ddf23cadcefc50dfd41692f07ca9d87276c9ee
KWB35 <kianbutts@icloud.com>
2025-05-08 17:04:34 +0100

    Continuation of week 3 work to be completed

--- Starter Pack/Starter Pack/Week 3 - Strings/main.cpp
+++ Starter Pack/Starter Pack/Week 3 - Strings/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
 
 int main()
 {
+    string sUserInput;
+    
+    cout << "Please enter your chosen phrase." << endl;
+    cin >> sUserInput;
+    size_t foundHyphen = sUserInput.find("-");
+    sUserInput.replace(foundHyphen,1," ");//replaces found dashes with a space
+
+    size_t foundpunctuation = sUserInput.find("!" && ";" && "," && "." && "/" && "?" && ")" && "(" && "[" && "]" && "{" && "}" && "`" && ":");
+    sUserInput.replace(foundpunctuation, 1, " ");
+     
+
+
+    cout << sUserInput << endl;
+
+    system("pause");
     
 }
--- Starter Pack/Starter Pack/Week 3 - Times Tables/main.cpp
+++ Starter Pack/Starter Pack/Week 3 - Times Tables/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	int aiTimesTableGrid[12][12];
-	for (i = 0; i < 12; i++)
+	int aiTimesTableGrid[12][12]; ///sets an array at the 12x12 size
+	for (int i = 0; i < 12; i++) 
+	{
+		cout << "|   ";//buffer to help with the readability abd user experince //still needs tweaks
+		for (int j = 0; j < 12; j++) //nested loop to make it loop 12 times for this section for every single loop of the out loop
+		{
+			aiTimesTableGrid[i][j] = (j + 1) * (i + 1);//multiplies to find teh correct vaue for each location in the grid
+			cout << aiTimesTableGrid[i][j] << "   |   ";//prints the grid with a buffer after each digit to increase its readability
+			
+		}
+		cout << endl;
+	}
+	system("pause");
 	return 0;
 }
 
